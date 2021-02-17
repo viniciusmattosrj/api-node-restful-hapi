7d97ca66f8b8fb83233d182dcd89b1aea7981a13
Vinicius Mattos <vinimattos.rj@gmail.com>
2021-02-17 13:45:54 -0300

    refactor: refatorando e extraindo para uma function trecho de transformar o response da api

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -1,9 +1,22 @@
 const ProductModel = require('../models/product');
 const statusCode = require('../utils/statuscode');
 
+const transformer = product => ({
+    type: 'products',
+    id: product.id,
+    attributes: {
+        name: product.name,
+        price: product.price,
+    },
+    links: {
+        self: `/api/v1/products/${product.id}`
+    }
+});
+
+
 const getAll = async (request, h) => {
     const products = await ProductModel.find({});
-    return products;
+    return products.map(transformer);
 };
 
 const save = async (req, h) => {
@@ -14,19 +27,7 @@ const save = async (req, h) => {
     product.price = price
     await product.save();
 
-    const prod = {
-        type: 'products',
-        id: product.id,
-        attributes: {
-            name: product.name,
-            price: product.price,
-        },
-        links: {
-            self: `/api/v1/products/${product.id}`
-        }
-    };
-
-    return h.response({ data: product }).code(statusCode.CREATED);
+    return h.response(transformer(product)).code(statusCode.CREATED);
 };
 
 module.exports = {
